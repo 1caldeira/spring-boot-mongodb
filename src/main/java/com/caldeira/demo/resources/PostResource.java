f72f4e3511a268b5d69e97f8614ad7946cc19b34
1caldeira <g.caldeiraa1@gmail.com>
2024-09-06 01:38:07 -0300

    Get post by ID Request

---
+++ src/main/java/com/caldeira/demo/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.caldeira.demo.resources;
+
+import com.caldeira.demo.domain.Post;
+
+import com.caldeira.demo.service.PostService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+}
---
+++ src/main/java/com/caldeira/demo/service/PostService.java
@@ -0,0 +1,37 @@
+package com.caldeira.demo.service;
+
+import com.caldeira.demo.domain.Post;
+import com.caldeira.demo.repository.PostRepository;
+import com.caldeira.demo.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Optional<Post> post = repo.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Object not found! Request Id: "+id));
+    }
+
+//    public Post insert(Post obj){
+//        return repo.insert(obj);
+//    }
+//
+//    public void delete(String id){
+//        findById(id);
+//        repo.deleteById(id);
+//    }
+//
+//    public void update(UserUpdateDTO dto){
+//        Post entity = repo.findById(dto.id()).orElseThrow(() -> new ObjectNotFoundException("Object not found! Id: "+dto.id()));
+//    }
+
+
+
+}
